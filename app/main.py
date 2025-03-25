8973333a64e72b5ea49bc356fce486919ae131d1
soyo-kaze <07sameersaini@gmail.com>
2025-03-26 00:04:23 +0530

    Add middleware for cors

--- app/main.py
+++ app/main.py
@@ -1,13 +1,8 @@
 from typing import Annotated
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from passlib.context import CryptContext
-from datetime import timedelta, datetime
-import jwt
-from jwt.exceptions import InvalidTokenError
+from fastapi import FastAPI, Depends
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import health_check, auth
 from app.database import lifespan
@@ -22,121 +17,18 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["GET", "OPTIONS"],
+    allow_headers=["*"],
+)
+
 app.include_router(health_check.router)
 app.include_router(auth.router)
 
 
-@app.get("/items/")
+@app.get("/token/")
 async def root(token: Annotated[str, Depends(oauth2_scheme)]):
     return {"token": token}
-
-
-# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-# class TokenResponse(BaseModel):
-#     access_token: str
-#     token_type: str
-
-
-# class TokenData(BaseModel):
-#     username: str | None = None
-
-
-# class UserModel(BaseModel):
-#     username: str
-#     email: str | None = None
-#     full_name: str | None = None
-#     disabled: bool = False
-
-
-# class UserInDB(UserModel):
-#     hashed_password: str
-
-
-# def verify_password(plain_password: str, hashed_password: str):
-#     return pwd_context.verify(plain_password, hashed_password)
-
-
-# def get_password_hash(password: str):
-#     return pwd_context.hash(password)
-
-
-# def get_user(db, username):
-#     if username in db:
-#         return UserInDB(**db.get(username))
-
-
-# def authenticate_user(db, username: str, password: str):
-#     user = get_user(db, username)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
-
-
-# def create_access_token(data: dict, expires_delta: timedelta | None = None):
-#     to_encode = data.copy()
-#     if expires_delta:
-#         expire = datetime.now() + expires_delta
-#     else:
-#         expire = datetime.now() + timedelta(minutes=15)
-
-#     to_encode.update({"exp": expire})
-#     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-#     return encoded_jwt
-
-
-# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Invalid authentication credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = TokenData(username=username)
-#     except InvalidTokenError:
-#         raise credentials_exception
-
-#     user = get_user(fake_users_db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-
-# async def get_current_active_user(
-#     user: Annotated[UserModel, Depends(get_current_user)],
-# ):
-#     if user.disabled:
-#         raise HTTPException(
-#             status_code=status.HTTP_400_BAD_REQUEST, detail="Inactive user"
-#         )
-#     return user
-
-
-# @app.get("/users/me")
-# async def root(current_user: Annotated[UserModel, Depends(get_current_active_user)]):
-#     return current_user
-
-
-# @app.post("/login")
-# async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
-#     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-#     if not user:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED,
-#             detail="Incorrect username or password",
-#             headers={"WWW-Authenticate": "Bearer"},
-#         )
-#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-#     access_token = create_access_token(
-#         data={"sub": user.username}, expires_delta=access_token_expires
-#     )
-
-#     return TokenResponse(access_token=access_token, token_type="bearer")
